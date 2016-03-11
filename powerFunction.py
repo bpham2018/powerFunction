468fb4626576f169780ce92378e84b7015cb8987
Bao Pham <BariumOxide@BaoPham.local>
2016-03-10 19:15:43 -0500

    Added comments. Checked program to make sure it still worked

--- powerFunction.py
+++ powerFunction.py
@@ -1,38 +1,39 @@
-def power( x, y ):
+def power( x, y ): # function will take two variables
 	
 	base = int( x )
 	
 	exponent = int( y )
 	
-	if exponent == 0:
+	if exponent == 0: # any number to the power of 0 is 1
 	
 		product = 1
 	
-	if exponent == 1:
+	if exponent == 1: # any number to the power of 1 stays the same
 	
 		product = int( base )
 		
-	if exponent > 1:
+	if exponent >= 2: 
 	
-		loopNumber = 2
+		loopNumber = 2 # starts at two because exponent starts at 2 too
 	
-		constantBase = int( base )
+		constantBase = int( base ) # need a base to stay the same so can keep multiplying by it
 	
-		while loopNumber <= exponent:
+		while loopNumber <= exponent: # loop that keeps multiply the base
 		
-			base = base * constantBase
+			base = base * constantBase # again base is changed by constantBase is not
 		
-			loopNumber += 1
+			loopNumber += 1 # add to loopNumber so it will eventually become greater than the exponent, 
+			# terminating the loop
 		
 		product = int( base )
 	
-	return "->" + str(product)
+	return "->" + str(product) # returns product with an arrow
 	
 #---------------------------------------------------------------------------------------------------
 
-x = raw_input( "Enter the base number: " )
+x = raw_input( "Enter the base number: " ) # getting user to enter a base number
 
-y = raw_input( "Enter the exponent: " )
+y = raw_input( "Enter the exponent: " ) # getting user to enter an exponent
 
-print( power( x, y ) )
+print( power( x, y ) ) # prints the product
 
